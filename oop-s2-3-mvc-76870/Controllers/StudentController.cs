6a9eefe804ab778622642f37d27c255192434ee9
Calisa-nnah <76870@student.dorset-college.ie>
2026-04-06 15:06:44 +0100

    student controller added

---
+++ oop-s2-3-mvc-76870/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VgcCollege.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
