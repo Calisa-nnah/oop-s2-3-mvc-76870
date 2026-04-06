b94b152ddfddd2860a97d132a8cf4432177605cd
Calisa-nnah <76870@student.dorset-college.ie>
2026-04-06 08:48:03 +0100

    Initial commit - project setup

---
+++ oop-s2-3-mvc-76870/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace oop_s2_3_mvc_76870.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
