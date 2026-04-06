b6e1afd3aaccd88022f969cfdc2fb142b1d75dc7
Calisa-nnah <76870@student.dorset-college.ie>
2026-04-06 11:01:45 +0100

    updated BranchesController

--- oop-s2-3-mvc-76870/Controllers/BranchesController.cs
+++ oop-s2-3-mvc-76870/Controllers/BranchesController.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using VgcCollege.Data;
 using VgcCollege.Models;
@@ -17,6 +16,98 @@ namespace VgcCollege.Controllers
             _context = context;
         }
 
-        // scaffolded actions...
+        // GET: Branches
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Branches.ToListAsync());
+        }
+
+        // GET: Branches/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var branch = await _context.Branches
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (branch == null) return NotFound();
+
+            return View(branch);
+        }
+
+        // GET: Branches/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Branches/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Branch branch)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(branch);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(branch);
+        }
+
+        // GET: Branches/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var branch = await _context.Branches.FindAsync(id);
+            if (branch == null) return NotFound();
+
+            return View(branch);
+        }
+
+        // POST: Branches/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Branch branch)
+        {
+            if (id != branch.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                _context.Update(branch);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(branch);
+        }
+
+        // GET: Branches/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var branch = await _context.Branches
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (branch == null) return NotFound();
+
+            return View(branch);
+        }
+
+        // POST: Branches/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var branch = await _context.Branches.FindAsync(id);
+            if (branch != null)
+            {
+                _context.Branches.Remove(branch);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
