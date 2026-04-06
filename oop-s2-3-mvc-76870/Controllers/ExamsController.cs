c57686f501060fc52000bd9636999c4b2e1d20e1
Calisa-nnah <76870@student.dorset-college.ie>
2026-04-06 13:59:49 +0100

    Added ExamsControllers

---
+++ oop-s2-3-mvc-76870/Controllers/ExamsController.cs
@@ -0,0 +1,140 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using VgcCollege.Data;
+using VgcCollege.Models;
+
+namespace VgcCollege.Controllers
+{
+    [Authorize(Roles = "Admin,Faculty")]
+    public class ExamsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ExamsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Exams
+        public async Task<IActionResult> Index()
+        {
+            var exams = _context.Exams
+                .Include(e => e.Course)
+                    .ThenInclude(c => c.Branch);
+
+            return View(await exams.ToListAsync());
+        }
+
+        // GET: Exams/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var exam = await _context.Exams
+                .Include(e => e.Course)
+                    .ThenInclude(c => c.Branch)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (exam == null) return NotFound();
+
+            return View(exam);
+        }
+
+        // GET: Exams/Create
+        public IActionResult Create()
+        {
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Name");
+            return View();
+        }
+
+        // POST: Exams/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Exam exam)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(exam);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Name", exam.CourseId);
+            return View(exam);
+        }
+
+        // GET: Exams/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam == null) return NotFound();
+
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Name", exam.CourseId);
+            return View(exam);
+        }
+
+        // POST: Exams/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Exam exam)
+        {
+            if (id != exam.Id) return NotFound();
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exam);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.Exams.Any(e => e.Id == exam.Id))
+                    {
+                        return NotFound();
+                    }
+                    throw;
+                }
+
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["CourseId"] = new SelectList(_context.Courses, "Id", "Name", exam.CourseId);
+            return View(exam);
+        }
+
+        // GET: Exams/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var exam = await _context.Exams
+                .Include(e => e.Course)
+                    .ThenInclude(c => c.Branch)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (exam == null) return NotFound();
+
+            return View(exam);
+        }
+
+        // POST: Exams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var exam = await _context.Exams.FindAsync(id);
+            if (exam != null)
+            {
+                _context.Exams.Remove(exam);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
